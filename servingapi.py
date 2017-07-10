f355542026de8dc2b4065566aea033a60a9632e3
hwanim <imputer2@gmail.com>
2017-07-10 23:05:10 +0900

    modify api file

--- servingapi.py
+++ servingapi.py
@@ -14,7 +14,7 @@ IMG_SIZE = 50
 LR = 1e-3
 
 MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '6conv-basic') # just so we remember which saved model is which, sizes must match
-TEST_DIR = ????
+# TEST_DIR = ????
 
 
 if os.path.exists('{}.meta'.format(MODEL_NAME)):
