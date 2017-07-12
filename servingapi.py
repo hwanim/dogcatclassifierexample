02e4e49d7cafcc73f2cbd93b260ca33e74489aed
hwanim <imputer2@gmail.com>
2017-07-12 18:24:02 +0900

    a

--- servingapi.py
+++ servingapi.py
@@ -31,6 +31,7 @@ def catdogclassifiation(message):
     url = message["attachment"][0]["contentUrl"]
     data = url2img(url)
     data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    model = model_load()
     model_out = model.predict([data])
     if np.argmax(model_out) == 1: return 'Dog'
     else: return 'Cat'
@@ -43,9 +44,43 @@ def url2img(url):
     img = np.array(img)
     return img
 
-if os.path.exists('{}.meta'.format(MODEL_NAME)):
-    model = model.load(MODEL_NAME)
 
+def model_load():
+    tf.reset_default_graph()
+
+
+    convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+    if os.path.exists('{}.meta'.format(MODEL_NAME)):
+        model = model.load(MODEL_NAME)
+
+    return model
 
 
 # def process_test_data():
